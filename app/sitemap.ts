3c269b845a26d70af66ac51358a75c07e847ccf0
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-13 09:29:11 +0000

    changed favicon and fixed sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,10 @@
 import { allPostsQuery } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client"; // Adjust this import based on your Sanity client setup
 import { MetadataRoute } from "next";
-import post from "@/sanity/schemas/documents/post";
+
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = "https://successsecretsbet.com"; // Replace with your actual website URL
+  const baseUrl = "https://pawfectdaily.com"; // Replace with your actual website URL
 
   // Define static routes
   const staticRoutes: MetadataRoute.Sitemap = [
@@ -24,11 +24,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   // Generate dynamic routes for posts
   const postRoutes: MetadataRoute.Sitemap = posts.map((post) => ({
-    url: `${baseUrl}/${post.slug}`,
+    url: `${baseUrl}/${post.slug?.current}`,
     lastModified: new Date(post._updatedAt),
   }));
-    console.log(posts);
-    
+  //console.log(posts);
 
   // Combine static and dynamic routes
   return [...staticRoutes, ...postRoutes];

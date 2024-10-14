19a339d7b6e2cd7a68a40edd11df889072a666fa
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-14 17:00:19 +0000

    removed nonexistent about page from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,10 +11,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: baseUrl,
       lastModified: new Date(),
     },
-    {
+    /*{
       url: `${baseUrl}/about`,
       lastModified: new Date(),
-    },
+    },*/
     // Add more static routes as needed
   ];
 

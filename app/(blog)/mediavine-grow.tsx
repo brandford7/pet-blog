fc3a1b7de9be3735b0b82cb3ebec5c5adb467bcc
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-15 09:51:20 +0000

    reverted to old script

--- app/(blog)/mediavine-grow.tsx
+++ app/(blog)/mediavine-grow.tsx
@@ -1,7 +1,5 @@
 import Script from "next/script";
 
-/*import Script from "next/script";
-
 const GrowScript = () => {
   return (
     <Script
@@ -26,23 +24,3 @@ const GrowScript = () => {
 };
 
 export default GrowScript;
-*/
-export default function GrowScript() {
-  return (
-    <>
-      <Script
-        id="grow-script"
-        strategy="afterInteractive"
-        src="https://faves.grow.me/main.js"
-        data-grow-faves-site-id="U2l0ZTphZGQzM2EyZS00OWJjLTQzMzctOGNkOS03NGIxY2M4ZDg2ZTM="
-      />
-      <Script id="grow-init" strategy="afterInteractive">
-        {`
-          window.growMe = window.growMe || function(e) {
-            (window.growMe._ = window.growMe._ || []).push(e);
-          };
-        `}
-      </Script>
-    </>
-  );
-}

3999324953d9259e03ea6561aba97f2c755c461d
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-17 16:22:15 +0000

    trying new implementation of grow script

--- app/(blog)/mediavine-grow.tsx
+++ app/(blog)/mediavine-grow.tsx
@@ -3,22 +3,33 @@ import Script from "next/script";
 const GrowScript = () => {
   return (
     <Script
-      type="text/javascript"
       id="grow-script"
-      strategy="beforeInteractive"
+      strategy="lazyOnload"
       data-grow-initializer=""
       dangerouslySetInnerHTML={{
-        __html: `!(function(){
-        window.growMe||((window.growMe=function(e){
-        window.growMe._.push(e)
-        ;}),(window.growMe._=[]));
-        var e=document.createElement("script");
-        (e.type="text/javascript"),
-        (e.src="https://faves.grow.me/main.js"),
-        (e.defer=!0),
-        e.setAttribute("data-grow-faves-site-id","U2l0ZTo5NWQzZjFhYi1jYjYzLTQ0NWQtYTVmOC1mNWEyYmU5OGVmOGQ=");
-        var t=document.getElementsByTagName("script")[0];
-        t.parentNode.insertBefore(e,t);})();`,
+        __html: `
+
+!(function () {
+
+window.growMe || ((window.growMe = function (e) { window.growMe._.push(e); }), (window.growMe._ = []));
+
+var e = document.createElement("script");
+
+e.type = "text/javascript";
+
+e.src = "https://faves.grow.me/main.js";
+
+e.defer = true;
+
+e.setAttribute("data-grow-faves-site-id", "U2l0ZTo5NWQzZjFhYi1jYjYzLTQ0NWQtYTVmOC1mNWEyYmU5OGVmOGQ=");
+
+var t = document.getElementsByTagName("script")[0];
+
+t.parentNode.insertBefore(e, t);
+
+})();
+
+`,
       }}
     />
   );

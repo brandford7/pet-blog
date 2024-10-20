4067628c18c47564cc9205aa4afd77591e2c535d
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-20 13:03:34 +0000

    removed the head implementation

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -103,10 +103,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} bg-white text-black`}>
-      <head>
-        <GrowScript />
-      </head>
       <body>
+        <script
+          data-grow-initializer=""
+          dangerouslySetInnerHTML={{
+            __html: `
+          !(function(){
+            window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));
+            var e=document.createElement("script");
+            (e.type="text/javascript"),
+            (e.src="https://faves.grow.me/main.js"),
+            (e.defer=true),
+            e.setAttribute("data-grow-faves-site-id","U2l0ZTo5NWQzZjFhYi1jYjYzLTQ0NWQtYTVmOC1mNWEyYmU5OGVmOGQ=");
+            var t=document.getElementsByTagName("script")[0];
+            t.parentNode.insertBefore(e,t);
+          })();
+        `,
+          }}
+          suppressHydrationWarning
+        />
         <section className="min-h-screen">
           {draftMode().isEnabled && <AlertBanner />}
           <main>{children}</main>

7b5e1badd05516ee321266285f13418fdd2767ca
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-20 13:13:01 +0000

    reimplemented grow script using component

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -104,26 +104,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} bg-white text-black`}>
       <body>
-        <Script
-          id="grow-script"
-          strategy="lazyOnload"
-          data-grow-initializer=""
-          dangerouslySetInnerHTML={{
-            __html: `
-          !(function(){
-            window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));
-            var e=document.createElement("script");
-            (e.type="text/javascript"),
-            (e.src="https://faves.grow.me/main.js"),
-            (e.defer=true),
-            e.setAttribute("data-grow-faves-site-id","U2l0ZTo5NWQzZjFhYi1jYjYzLTQ0NWQtYTVmOC1mNWEyYmU5OGVmOGQ=");
-            var t=document.getElementsByTagName("script")[0];
-            t.parentNode.insertBefore(e,t);
-          })();
-        `,
-          }}
-          suppressHydrationWarning
-        />
+       <GrowScript/>
         <section className="min-h-screen">
           {draftMode().isEnabled && <AlertBanner />}
           <main>{children}</main>

ef42d46dff415ba161c30b2255ae75139ec488cf
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-20 13:02:30 +0000

    added change frequency to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,16 @@ import { allPostsQuery } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client"; // Adjust this import based on your Sanity client setup
 import { MetadataRoute } from "next";
 
+type ExtendedSitemapEntry = MetadataRoute.Sitemap[number] & {
+  changeFrequency?:
+    | "always"
+    | "hourly"
+    | "daily"
+    | "weekly"
+    | "monthly"
+    | "yearly"
+    | "never";
+};
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://pawfectdaily.com"; // Replace with your actual website URL
 
@@ -10,6 +20,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: baseUrl,
       lastModified: new Date(),
+      changeFrequency: "daily",
     },
     /*{
       url: `${baseUrl}/about`,
@@ -25,8 +36,8 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const postRoutes: MetadataRoute.Sitemap = posts.map((post) => ({
     url: `${baseUrl}/posts/${post.slug?.current}`,
     lastModified: new Date(post._updatedAt),
+    changeFrequency: "daily",
   }));
-  
 
   // Combine static and dynamic routes
   return [...staticRoutes, ...postRoutes];

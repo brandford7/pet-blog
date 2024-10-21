329802be253f4f86a29ee364e4d5108eb219d263
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-21 17:36:00 +0000

    added hydro online ads new implementation

--- app/(blog)/hydro-ads.tsx
+++ app/(blog)/hydro-ads.tsx
@@ -3,22 +3,24 @@ import React from "react";
 
 const HydroAds = () => {
   return (
-    <div>
+    <>
       <Script
-        id="hydro_config"
-        strategy="afterInteractive"
+        id="hydro-tracking"
+        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            window.Hydro_tagId = "bd50ff4d-4a65-4a09-be2c-a707e30fb646";
-          `,
+          window.Hydro_tagId = "bd50ff4d-4a65-4a09-be2c-a707e30fb646";
+          (function(h,y,d,r,o){
+            h[d]=h[d]||function(){(h[d].q=h[d].q||[]).push(arguments)};
+            var s=y.createElement('script');
+            s.async=1;
+            s.src='https://track.hydro.online/';
+            y.head.appendChild(s);
+          })(window,document,'Hydro');
+        `,
         }}
       />
-      <Script
-        id="hydro_script"
-        src="https://track.hydro.online/"
-        strategy="beforeInteractive"
-      />
-    </div>
+    </>
   );
 };
 
